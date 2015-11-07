829ec9c69df78fa232acd00dce948f183a525559
automotua <adam.junqiang.li@gmail.com>
2015-11-06 19:03:59 -0500

    added UserService

---
+++ ApacheCMDA-Frontend/app/models/metadata/UserService.java
@@ -0,0 +1,15 @@
+package models.metadata;
+
+import models.User;
+
+public class UserService {
+	
+	public boolean verifyUserAuthentity(String email, String password) {
+		return false;
+	}
+	
+	public User getUserByEmail(String email) {
+		return null;
+	}
+
+}
